47c2255896d4e17e1d07c674765c2b10df692583
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-02-23 21:31:48 +0800

    add dropdown show for blog

--- js/main_helpers.js
+++ js/main_helpers.js
@@ -1,8 +1,13 @@
 function navbarFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function blogDropdown(){
+    document.getElementById('blog-dropdown-content').
+      classList.toggle('show');
 }
