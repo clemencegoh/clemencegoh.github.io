4b9a9dfffd9a11f07684398e50e3b6873ea0a0b1
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-03-04 16:46:17 +0800

    add start of blogging and notes area

---
+++ js/blog/data.js
@@ -0,0 +1,24 @@
+function getBlogData(){
+	return [
+		{
+			Title: "",
+			Images: [""],
+			Content: "",
+		},
+	];
+}
+
+function getNotesData(){
+	return [
+		{
+			Title: "Deep Learning",
+			Image: "",
+			Lectures: "",
+			Notes: "",
+			Summary: "Deep Learning application and theory, SUTD course.",
+			StartDate: new Date(),
+			EndDate: new Date(),  // leave empty if incomplete
+			Completed: false,
+		},
+	];
+}
---
+++ js/skills/data.js
@@ -0,0 +1,63 @@
+function getSkillsData(){
+	let skills1 = [
+		{
+			Name: "Python",
+			Time_Start: new Date(2017, 0),
+			Projects: 7,
+			Proficiency: "Advanced",
+		},
+		{
+			Name: "Golang",
+			Time_Start: new Date(2017, 4),
+			Projects: 3,
+			Proficiency: "Intermediate",
+		},
+		{
+			Name: "Java",
+			Time_Start: new Date(2017, 8),
+			Projects: 2,
+			Proficiency: "Intermediate",
+		},
+		{
+			Name: "Blockchain",
+			Time_Start: new Date(2018, 8),
+			Projects: 1,
+			Proficiency: "Basic",
+		},
+		{
+			Name: "Android",
+			Time_Start: new Date(2017, 8),
+			Projects: 1,
+			Proficiency: "Basic",
+		},
+	];
+
+	let skills2 = [
+		{
+			Name: "Javascript",
+			Time_Start: new Date(2018, 4),
+			Projects: 4,
+			Proficiency: "Novice"
+		},
+		{
+			Name: "HTML/CSS",
+			Time_Start: new Date(2018, 4),
+			Projects: 3,
+			Proficiency: "Novice"
+		},
+		{
+			Name: "Reactjs",
+			Time_Start: new Date(2018, 4),
+			Projects: 1,
+			Proficiency: "Basic",
+		},
+		{
+			Name: "AI",
+			Time_Start: new Date(2018, 8),
+			Projects: 2,
+			Proficiency: "Basic",
+		}
+	];
+
+	return [skills1, skills2]
+}
