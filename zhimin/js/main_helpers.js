a0aa6e3f8d8f86aeb024297584a45b7c123e1780
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-11-07 00:57:49 +0800

    initial filling out of details for zhimin, require completion of templating with js

---
+++ zhimin/js/main_helpers.js
@@ -0,0 +1,62 @@
+
+function navbarFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav sticky") {
+        x.className += " responsive";
+    } else {
+	    x.className = "topnav sticky";
+    }
+}
+
+function removeResponsive() {
+	let x = document.getElementById("myTopnav");
+	x.className = "topnav sticky";
+}
+
+// Toggler functions for enabling changes to happen on click
+function navbarToggler(header_name, selections_name) {
+	let header = document.getElementById(header_name);
+	let btns = header.getElementsByClassName(selections_name);
+
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", function() {
+			let current = header.getElementsByClassName("active");
+			current[0].className = current[0].className.replace(" active", "");
+			this.className += " active";
+			removeResponsive();
+		});
+	}
+}
+
+
+function projectsFilterToggler(header_name, selections_name) {
+	let projects_area = document.getElementById('projects_content');
+	let header = document.getElementById(header_name);
+	let btns = header.getElementsByClassName(selections_name);
+
+	for (let i = 0; i < btns.length; i++) {
+		// Add function to click
+		btns[i].addEventListener("click", function() {
+			// sets this to active while the rest to not active
+			let current = header.getElementsByClassName("active");
+			current[0].className = current[0].className.replace(" active", "");
+			this.className += " active";
+			let filtering_for = this.innerText.toLowerCase();
+			let to_filter = projects_area.getElementsByClassName('all');
+
+			for (let j=0;j<to_filter.length;j++){
+				if (!to_filter[j].className.includes(filtering_for)){
+					to_filter[j].setAttribute('hidden', true);
+				}else{
+					to_filter[j].removeAttribute('hidden');
+				}
+			}
+		});
+	}
+}
+
+function goto_Section(section) {
+	window.scrollTo(0, document.getElementsByClassName(section)[0].offsetTop);
+}
+
+
