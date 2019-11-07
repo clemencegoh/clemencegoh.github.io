6f0c59fa41addd2b3de9c1e937a15934ec2caf7d
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-11-07 13:48:00 +0800

    add initial templating for projects data

---
+++ zhimin/data/project_data.js
@@ -0,0 +1,20 @@
+function getProjectsData() {
+	// add accordingly, array of json data
+	let projectsData = [
+		{
+			Description: "First of the Emilia series from HappyBloooms",
+			Category: "happybloooms",
+			Link: "https://www.instagram.com/p/B4hEPKegbx5/",  // todo: change this to correct
+			Preview: "files/zhimin_happybloooms_emilia1.jpg",
+			PrivateName: "Emilia Series @HappyBloooms"
+		},
+		{
+			Description: "Second of the Emilia series from HappyBloooms",
+			Category: 'happybloooms',
+			Link: "https://www.instagram.com/p/B4hEPKegbx5/",
+			Preview: "files/zhimin_happybloooms_emilia2.jpg",
+			PrivateName: "Emilia Series @HappyBlooms"
+		},
+	];
+	return projectsData;
+}
--- zhimin/js/main_templating.js
+++ zhimin/js/main_templating.js
@@ -2,24 +2,27 @@ function generateProjects(){
     let projectsData = getProjectsData();
 
     // init areas
-    let projectsArea = document.getElementById("projects_content");
-    let proj_templ = document.getElementsByTagName("template")[0];
+    let projectsArea = document.getElementById("portfolio-build-section");
+    let projectsArea2 = document.getElementById('portfolio-build-section2');
+    let proj_templ = document.getElementsByTagName("template")[0];  // portfolio template
 
     // clear area
     projectsArea.textContent = '';
+    projectsArea2.textContent = '';
 
     // init template pointers
-    let proj_fields = proj_templ.content.getElementById("filter_fields");
-    let img_preview = proj_templ.content.getElementById("proj_preview_img");
-    let proj_link = proj_templ.content.getElementById("proj_link");
-    let proj_decription = proj_templ.content.getElementById("proj_description");
-    let proj_category = proj_templ.content.getElementById("proj_category");
+    let proj_label = proj_templ.content.getElementById("project-label");
+    let img_preview = proj_templ.content.getElementById("project-image");
+    let proj_link = proj_templ.content.getElementById("project-link");
+    let proj_decription = proj_templ.content.getElementById("project-description");
+    let proj_category = proj_templ.content.getElementById("project-single-wrapper");
 
 
     for (let i=0;i<projectsData.length;i++){
         let d = projectsData[i];
-        proj_fields.setAttribute('class', 'single-portfolio col-sm-4 all ' + d.Category);
-        proj_category.innerText = d.Category;    
+        proj_category.setAttribute('class', 'w3-third w3-container w3-margin-bottom all ' + d.Category);
+        // proj_category.innerText = d.Category;
+		proj_label.innerHTML = '<b>' + d.PrivateName + '</b>';
         img_preview.setAttribute('src', d.Preview);
         proj_link.setAttribute('href', d.Link);
         proj_decription.innerText = d.Description;  
@@ -49,45 +52,43 @@ function timeSince(date) {
 	return year + " years " + month + " months, ";
 }
 
-function generateSkills(){
-	let data = getSkillsData();
-
-	// init areas
-	let skills_templ = document.getElementsByTagName("template")[1];
-
-	// clear area
-	let area1 = document.getElementById("skills-area1");
-	let area2 = document.getElementById("skills-area2");
-	area1.textContent = '';
-	area2.textContent = '';
-
-	// init template pointers
-	let name = skills_templ.content.getElementById("skill-desc");
-	let details = skills_templ.content.getElementById("skill-details");
-	let proficiency = skills_templ.content.getElementById("skill-proficiency");
-
-	for (let i=0;i<data.length;i++){
-		for (let j=0;j<data[i].length;j++){
-			let d = data[i][j];
-
-			console.log('doing for data:', d);
-
-			name.innerText = d.Name;
-			details.innerText = timeSince(d.Time_Start) + d.Projects + " Projects";
-			proficiency.setAttribute('class', 'single-skill ' + d.Proficiency);
-			proficiency.innerText = d.Proficiency;
-
-			// clone
-			let skills_node = skills_templ.content.cloneNode(true);
-
-			if (i===0){
-				area1.appendChild(skills_node);
-			}else{
-				area2.appendChild(skills_node);
-			}
-
-		}
-	}
-
-	console.log('completed creating of skills');
-}
+// function generateSkills(){
+// 	let data = getSkillsData();
+//
+// 	// init areas
+// 	let skills_templ = document.getElementsByTagName("template")[1];
+//
+// 	// clear area
+// 	let area1 = document.getElementById("skills-section");
+// 	area1.textContent = '';
+//
+// 	// init template pointers
+// 	let name = skills_templ.content.getElementById("skill-desc");
+// 	let details = skills_templ.content.getElementById("skill-details");
+// 	let proficiency = skills_templ.content.getElementById("skill-proficiency");
+//
+// 	for (let i=0;i<data.length;i++){
+// 		for (let j=0;j<data[i].length;j++){
+// 			let d = data[i][j];
+//
+// 			console.log('doing for data:', d);
+//
+// 			name.innerText = d.Name;
+// 			details.innerText = timeSince(d.Time_Start) + d.Projects + " Projects";
+// 			proficiency.setAttribute('class', 'single-skill ' + d.Proficiency);
+// 			proficiency.innerText = d.Proficiency;
+//
+// 			// clone
+// 			let skills_node = skills_templ.content.cloneNode(true);
+//
+// 			if (i===0){
+// 				area1.appendChild(skills_node);
+// 			}else{
+// 				area2.appendChild(skills_node);
+// 			}
+//
+// 		}
+// 	}
+//
+// 	console.log('completed creating of skills');
+//}
