83256f58e1a58b747eb45a06cb34dbb2f86d4b5f
Clemence Goh <clemencegoh@users.noreply.github.com>
2020-10-18 13:36:30 +0800

    Updates

---
+++ precache-manifest.bec0f303dfb2116c52b2ea9d61675cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d45561ab8f3867bf89c82dfe2ce6c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1229b223c8580b809fc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a972a2b4c4d8e9716514",
+    "url": "/static/js/2.197a0882.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.197a0882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1229b223c8580b809fc",
+    "url": "/static/js/main.c0a6d6ca.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
