fb763ffc290c85c2ca7c44bed2454526719f157a
Clemence Goh <clemencegoh@users.noreply.github.com>
2020-10-18 13:53:03 +0800

    test commit for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let name = "Clemence";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World! {name} project</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
