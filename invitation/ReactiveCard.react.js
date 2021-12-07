1523ac20465c53ad718aa1bbd4846fb73e25966f
Clemence Goh <clemencegoh@gmail.com>
2021-12-07 19:03:57 +0800

    create mobile view - left some css

--- invitation/ReactiveCard.react.js
+++ invitation/ReactiveCard.react.js
@@ -9,80 +9,89 @@ function ReactiveCard() {
 		setShowStart(false);
 	};
 
+	const languageCard = (
+		<article id="wrapper-card" className="wrapper-card">
+			<img src="./images/select-lang-avatar.png" />
+			<span></span>
+			<div className="card">
+				<div>
+					<h3>
+						<span className="top-break">Select Language</span>
+						<span>/ 选择语言：</span>
+					</h3>
+					<div className="lang-buttons">
+						<button onClick={() => chooseLang("eng")}>English</button>
+						<span className="divider"></span>
+						<button onClick={() => chooseLang("chi")}>中文</button>
+					</div>
+				</div>
+			</div>
+		</article>
+	);
+
 	/**
 	 * Language card
 	 */
 	if (showStart) {
-		return (
-			<article id="wrapper-card" className="wrapper-card">
-				<img src="./images/select-lang-avatar.png" />
-				<span></span>
-				<div className="card">
-					<div>
-						<h3>
-							<span className="top-break">Select Language</span>
-							<span>/ 选择语言：</span>
-						</h3>
-						<div className="lang-buttons">
-							<button onClick={() => chooseLang("eng")}>English</button>
-							<span className="divider"></span>
-							<button onClick={() => chooseLang("chi")}>中文</button>
-						</div>
-					</div>
-				</div>
-			</article>
-		);
+		return languageCard;
 	}
 
-	const engContent = () => {
-		return (
-			<div className="invitation-card">
-				<h1>Wedding of</h1>
-				<h2>Clemence and Zhi Min</h2>
-				<h3>8 Oct 2022</h3>
-				<h3>7pm</h3>
+	const engContent = (
+		<div className="invitation-card">
+			<h1>Wedding of</h1>
+			<h2>Clemence and Zhi Min</h2>
+			<h3>8 Oct 2022</h3>
+			<h3>7pm</h3>
 
-				<h4>Hilton Singapore Orchard</h4>
-				<h4>581 Orchard Rd, Singapore 238883</h4>
+			<h4>Hilton Singapore Orchard</h4>
+			<p>581 Orchard Rd, Singapore 238883</p>
 
-				<div>
-					<button className="rsvp-cta">RSVP Here</button>
-				</div>
+			<div>
+				<button className="rsvp-cta">RSVP Here</button>
 			</div>
-		);
-	};
+		</div>
+	);
 
-	const chineseContent = () => {
-		return (
-			<div className="invitation-card">
-				<h1>吴智权 和 梁智敏</h1>
-				<h3>谨订于</h3>
-				<h3>公历 2022 年 10 月 08 日</h3>
-				<h3>举行结婚喜宴</h3>
+	const chineseContent = (
+		<div className="invitation-card">
+			<h1>吴智权 和 梁智敏</h1>
+			<h3>谨订于</h3>
+			<h3>公历 2022 年 10 月 08 日</h3>
+			<h3>举行结婚喜宴</h3>
 
-				<h4>Hilton Singapore Orchard</h4>
-				<h4>581 Orchard Rd, Singapore 238883</h4>
+			<h4>Hilton Singapore Orchard</h4>
+			<h4>581 Orchard Rd, Singapore 238883</h4>
 
-				<div>
-					<button className="rsvp-cta">RSVP Here</button>
-				</div>
+			<p>中午七时晋席</p>
+			<p>您的光临将是</p>
+			<p>我们最大的荣幸</p>
+
+			<div>
+				<button className="rsvp-cta">登记出席</button>
 			</div>
-		);
-	};
+		</div>
+	);
 
 	// Invitation card
 	return (
-		<article id="wrapper-card" className="wrapper-card">
-			{lang === "eng" ? (
-				<img src="./images/main-avatar.png" />
-			) : (
-				<img src="./images/main-avatar-chinese.png" />
-			)}
-			<span></span>
-			<div className="card">
-				{lang === "eng" ? engContent() : chineseContent()}
+		<React.Fragment key={"wrapper-fragment"}>
+			<article id="wrapper-card" className="wrapper-card">
+				{lang === "eng" ? (
+					<img src="./images/main-avatar.png" />
+				) : (
+					<img src="./images/main-avatar-chinese.png" />
+				)}
+				<span></span>
+				<div className="card">
+					{lang === "eng" ? engContent : chineseContent}
+				</div>
+			</article>
+			<div className="language-select">
+				<button onClick={() => chooseLang("eng")}>English</button>
+				<hr className="line-divider" />
+				<button onClick={() => chooseLang("chi")}>中文</button>
 			</div>
-		</article>
+		</React.Fragment>
 	);
 }
 
