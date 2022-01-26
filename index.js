cbc56f2b8f16ee9f82b4f9063a5de7e6aedf91f3
Clemence Goh <clemencegoh@gmail.com>
2022-01-27 00:32:10 +0800

    remove unnecessary css for now

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const [projectsData, skillsData] = getData();
+console.log(projectsData, skillsData);
+
+/**
+ * Components
+ */
+{
+  function SomeComponent() {}
+
+  // const someContainer = document.querySelector("#");
+  // ReactDOM.render(React.createElement(SomeComponent), someContainer);
+}
